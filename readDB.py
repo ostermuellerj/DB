5a6414775ce86dc871fef43aac7224eb66e4c5fe
John Ostermueller <ostermuellerj@gmail.com>
2020-02-01 08:42:07 -0600

    add test input

--- readDB.py
+++ readDB.py
@@ -7,28 +7,29 @@ def main():
 		
 	print("\n\n\n")
 
-	Record = getRecord(f, 3)
-	print("\n\nCall-->getRecord(3)","\n\nOutput:",Record, "\n\n")
+	Record = get_record(f, 3)
+	print("\n\nCall-->get_record(3)","\n\nOutput:",Record, "\n\n")
 	
-	Record = getRecord(f, 9)
-	print("Call-->getRecord(9)","\n\nOutput:",Record, "\n\n")
-	
-	
-	Record = getRecord(f, 99999)
-	print("Call-->getRecord(99999)","\n\nOutput:",Record, "\n\n")
+	Record = get_record(f, 9)
+	print("Call-->get_record(9)","\n\nOutput:",Record, "\n\n")
 	
 	
-	Record = binarySearch(f, 4)
-	print("Call-->binarySearch(4)","\n\nOutput:",Record, "\n\n")
+	Record = get_record(f, 99999)
+	print("Call-->get_record(99999)","\n\nOutput:",Record, "\n\n")
 	
-	Record = binarySearch(f, 12)
-	print("Call-->binarySearch(12)","\n\nOutput:",Record, "\n\n")
+	
+	Record = binary_search(f, 4)
+	print("Call-->binary_search(4)","\n\nOutput:",Record, "\n\n")
+	
+	Record = binary_search(f, 12)
+	print("Call-->binary_search(12)","\n\nOutput:",Record, "\n\n")
 	
 	f.close()
 
 # Get record number n-th (from 1 to 4360)
 def get_record(f, recordNum):
-	record = "We only have 9 records, requested record NOT_FOUND"
+	#record = "We only have 9 records, requested record NOT_FOUND"
+	record = "requested record NOT_FOUND"
 	global num_records
 	global record_size
 	
@@ -44,12 +45,13 @@ def binary_search(f, num_id):
 	global num_records,record_size
 	low=0
 	high=num_records-1
-	record = "We only have 9 records, requested record NOT_FOUND"
+	#record = "We only have 9 records, requested record NOT_FOUND"
+	record = "requested record NOT_FOUND"
 	Found = False
 	
 	while not Found and high>=low and num_id<num_records:
 		middle = (low+high)/2
-		record = getRecord(f, int(middle+1))
+		record = get_record(f, int(middle+1))
 		middleidnum = record[0:5]
 		if int(middleidnum)== int(num_id):
 			Found=True
