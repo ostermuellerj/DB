7b7de71e5611fb01270568ea59fa044443898379
Gavin Glenn <43220039+veths@users.noreply.github.com>
2020-02-05 16:28:47 -0600

    yes

--- helpers.py
+++
@@ -1,8 +0,0 @@
-def fix_length(string, length):
-    string = string.rstrip(" \n")
-    if len(string) > length:
-        print("Something is seriously wrong here.")
-    else:
-        diff = length - len(string)
-        out_string = str(string) + " "*diff
-        return out_string
